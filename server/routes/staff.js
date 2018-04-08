0481040c40e43bf5b3014a8ae0d582840bb2eeb3
Guru Prasad G <guruprasad@gamenous.com>
2018-04-08 17:06:56 +0530

    staff signup added

--- server/routes/staff.js
+++ server/routes/staff.js
@@ -9,11 +9,14 @@ var dbconfig = require("../configs/database");
 var connection = mysql.createConnection(dbconfig.connection);
 connection.query("USE " + dbconfig.database);
 
+import { checkUsernameAlreadyExists, createUser } from '../controllers/staff';
+import { validateUserRegister } from '../../shared/validations/user/register';
+
 import serverConfig from '../configs/server';
 
-let routesAuth = express.Router();
+let routesStaff = express.Router();
 
-routesAuth.post('/', (request, response) => {
+routesStaff.post('/', (request, response) => {
     let responseData = {
         success: false,
         token: '',
@@ -46,6 +49,50 @@ routesAuth.post('/', (request, response) => {
             }
             return response.json(responseData);
         });
+});
+
+routesStaff.post('/', (request, response) => {
+    let responseData = {
+        success: false,
+        errors: {}
+    };
+    console.log(request.body);
+    checkUsernameAlreadyExists(request.body, (err, isTaken) => {
+        if (err) {
+            response.status(400);
+            responseData.errors = err;
+            return response.json(responseData)
+        }
+        console.log(isTaken);
+        if (!isTaken) {
+            let { errors, isValid } = validateUserRegister(request.body);
+            if (isValid) {
+                createUser(request.body, (err, result) => {
+                    if (err) {
+                        response.status(500);
+                        responseData.success = false;
+                        responseData.errors.form = "Creation failed";
+                        response.json(responseData);
+                    } else {
+                        responseData = result;
+                        responseData.success = true;
+                        response.status(201); // created status
+                        response.json(responseData);
+                    }
+                })
+            } else {
+                response.status(400);
+                responseData.success = false;
+                responseData.errors = errors;
+                response.json(responseData);
+            }
+        } else {
+            response.status(400);
+            responseData.success = false;
+            responseData.errors.form = "Username already taken";
+            response.json(responseData);
+        }
+    });
 });
 
 export default routesAuth;
